142c950adca193654e0f42731dfb4dbbc52efd80
alimert-t <turaclar.alimert@gmail.com>
2023-12-08 14:44:17 +0100

    exe.py: imports updated

--- exe.py
+++ exe.py
@@ -6,24 +6,27 @@ import os
 import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
-from amo.ktilde import ktilde
-from amo.tail import tail
-from amo.correction import recoilBroad
+from taillib.ktilde import ktilde
+from taillib.tail import tail
+from taillib.correction import recoilBroad
+from taillib.correction import shift_start_energy
+from taillib.bin import bin_distribution
 
-def calc(a, c, istep, corr, fname):
+def calc(isotopologue, estart, eend, corr, fname, bin_size):
+        
+    print("Calculation starts.")
+    print("")
 
-    # Hartree to eV conversion
     ehinev = 27.2113962
+    enanf = estart / ehinev
+    ethrsd = enanf
+    enend = eend / ehinev
 
-    # Conversion of threshold energy, start and end energies to eV 
-    ethrsd = a / ehinev
-    b = a
-    enanf = b / ehinev
-    enend = c / ehinev
-    # Calculating energy steps
-    enstep = (enend - enanf) / istep
+    enstep = 0.5 / ehinev 
+
     enueb = enanf - enstep
-    istep += 1
+
+    istep = int((enend-enanf) / enstep) + 1
 
     # Starting the calculation, results will be written into 'transition.txt'
     # 'transition.txt' is a scratch file, it will be deleted
@@ -47,42 +50,37 @@ def calc(a, c, istep, corr, fname):
             # Calculation of fractional recoil momentum with function ktilde
             fracRecoil = ktilde(enres)
             
-            print("{:.5f}".format(enres), "             ", "{:.7e}".format(probability), "             ", "{:.5f}".format(fracRecoil), file=fp)
+            print("{:.5f}".format(enres), "\t", "{:.7e}".format(probability), "\t", "{:.5f}".format(fracRecoil), file=fp)
     
-    header = ["Energy [eV]", "Prob. Density [eV^-1]", "Frac. Rec. Mom. [a_0^(-1)]"]    
-    
+    header = ["Energy [eV]", "Binned Prob.", "Frac. Rec. Mom. [a_0^(-1)]"]    
+
     # Checking if energy correction (fractional recoil momentum broadening)
     # will be aplied or not
     if corr:
         # Here, correction is applied (broadening)
-        dfResult = recoilBroad()
-        # Results are written into generated file_name
+        dfResult = pd.read_csv("transition" + '.txt', sep='\\s+', header=None)
+        dfResult = recoilBroad(isotopologue)
         dfResult.columns = ['Energy [eV]', 'Prob. Dist. [eV^-1]', 'Frac. Rec. Mom. [a_0^-1]']
-        dfResult.to_csv(f"out/{fname}.fsd", sep="\t", index=False, header=header)
-        print("")
-        print(f"Results have been written into {fname}.fsd file.")
-        print("")
-        print(dfResult)
-        print("")
-        print("exe.py and run_tail_ktilde.py executed gracefully.")
-        print("")
+
     else:
         # No correction applied
         dfResult = pd.read_csv("transition" + '.txt', sep='\\s+', header=None)
         dfResult.columns = ['Energy [eV]', 'Prob. Dist. [eV^-1]', 'Frac. Rec. Mom. [a_0^-1]']
-        # Results are written into generated file_name
-        dfResult.to_csv(f"out/{fname}_corrNO.fsd", sep="\t", index=False, header=header)
-        print("")
-        print(f"Results have been written into {fname}_corrNO.fsd file.")
-        print("")        
+        fname += "_noCorr"
         print(" ")
         print("Fractional recoil momentum shift is NOT applied!")
         print(" ")
-        print(dfResult)
-        print("")
-        print("exe.py and run_tail_ktilde.py executed gracefully.")
-        print("")
     
+    binned_final_distribution = bin_distribution(dfResult, bin_size)
+    binned_final_distribution['Frac. Rec. Mom. [a_0^-1]'] = binned_final_distribution['Energy [eV]'].apply(ktilde)
+    binned_final_distribution.to_csv(f"out/{fname}.fsd", sep="\t", index=False, header=header)
+    
+    print("")
+    print(f"Results have been written into {fname}.fsd file. \n")
+    print(binned_final_distribution)
+    print("exe.py and run_tail_ktilde.py executed gracefully.")
+    print("")
+
     # Scratch 'transition.txt' is removed, ready to be generated again for next calculations
     scratch = "transition.txt"
     os.remove(scratch)
