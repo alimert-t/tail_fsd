5028306304d1a67289f59318074a879a81a7159d
alimert-t <turaclar.alimert@gmail.com>
2023-12-24 23:01:53 +0300

    exe.py: ongoing calculations aren't written to a scratch txt         file anymore. Instead, data is being appended to a list.

--- exe.py
+++ exe.py
@@ -2,7 +2,6 @@
 
 import sys
 import math
-import os
 import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
@@ -12,7 +11,7 @@ from taillib.correction import recoilBroad
 from taillib.correction import shift_start_energy
 from taillib.bin import bin_distribution
 
-def calc(isotopologue, estart, eend, corr, fname, bin_size):
+def calc(isotopologue, estart, eend, corr, fname):
         
     print("Calculation starts.")
     print("")
@@ -29,66 +28,48 @@ def calc(isotopologue, estart, eend, corr, fname, bin_size):
         ethrsd = ionization_energies[isotopologue] / ehinev
 
     enstep = 0.5 / ehinev 
-
     enueb = enanf - enstep
-
     istep = int((enend-enanf) / enstep) + 1
 
-    # Starting the calculation, results will be written into 'transition.txt'
-    # 'transition.txt' is a scratch file, it will be deleted
-    with open("transition.txt", "w") as fp:
-        for i in range(1, int(istep)):
-            eprime = enueb - ethrsd
-            if eprime == 0:
-                eprime = 1.0e-12
-            elif eprime < 0:
-                eprime = 0.1e-12
+    result_data = []
 
-            enueb += enstep 
-            enres = enueb * ehinev
-            
-            # Calculating probabilities with tail function
-            probability = tail(eprime, ethrsd, enueb, enstep)
-            
-            if eprime == 0.1e-12:
-                probability += 0.000277
-
-            # Calculation of fractional recoil momentum with function ktilde
-            fracRecoil = ktilde(enres)
-            
-            print("{:.5f}".format(enres), "\t", "{:.7e}".format(probability), "\t", "{:.5f}".format(fracRecoil), file=fp)
+    for i in range(1, int(istep)):
+        eprime = enueb - ethrsd
+        if eprime <= 0:
+            eprime = 1.0e-12
+        enueb += enstep 
+        enres = enueb * ehinev
+        # Calculating probabilities with tail function
+        probability = tail(eprime, ethrsd, enueb, enstep)
+        # Calculation of fractional recoil momentum with function ktilde
+        fracRecoil = ktilde(enres)
+        result_data.append([enres, probability, fracRecoil])
     
-    header = ["Energy [eV]", "Binned Prob.", "Frac. Rec. Mom. [a_0^(-1)]"]    
+    header = ["Energy [eV]", "Prob. Density [eV^-1]", "Frac. Rec. Mom. [a_0^(-1)]"]    
+    dfResult = pd.DataFrame(result_data, columns=['e', 'p', 'k'])
 
     # Checking if energy correction (fractional recoil momentum broadening)
     # will be aplied or not
     if corr:
         # Here, correction is applied (broadening)
-        dfResult = pd.read_csv("transition" + '.txt', sep='\\s+', header=None)
-        dfResult = recoilBroad(isotopologue)
-        dfResult.columns = ['Energy [eV]', 'Prob. Dist. [eV^-1]', 'Frac. Rec. Mom. [a_0^-1]']
+        dfResult = recoilBroad(isotopologue, dfResult, header)
 
     else:
         # No correction applied
-        dfResult = pd.read_csv("transition" + '.txt', sep='\\s+', header=None)
-        dfResult.columns = ['Energy [eV]', 'Prob. Dist. [eV^-1]', 'Frac. Rec. Mom. [a_0^-1]']
         fname += "_noCorr"
         print(" ")
         print("Fractional recoil momentum shift is NOT applied!")
         print(" ")
     
-    binned_final_distribution = bin_distribution(dfResult, bin_size)
-    binned_final_distribution['Frac. Rec. Mom. [a_0^-1]'] = binned_final_distribution['Energy [eV]'].apply(ktilde)
-    binned_final_distribution.to_csv(f"out/{fname}.fsd", sep="\t", index=False, header=header)
+    #binned_final_distribution = bin_distribution(dfResult, bin_size)
+    #binned_final_distribution['Frac. Rec. Mom. [a_0^-1]'] = binned_final_distribution['Energy [eV]'].apply(ktilde)
+
+    dfResult.to_csv(f"out/{fname}.fsd", sep="\t", index=False, header=header)
     
     print("")
     print(f"Results have been written into {fname}.fsd file. \n")
-    print(binned_final_distribution)
+    print(dfResult)
     print("exe.py and run_tail_ktilde.py executed gracefully.")
     print("")
-
-    # Scratch 'transition.txt' is removed, ready to be generated again for next calculations
-    scratch = "transition.txt"
-    os.remove(scratch)
 
 #   END OF CODE
