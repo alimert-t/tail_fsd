153ac1a7948063b6ced2154f9b4ebb8b43c109a6
alimert-t <turaclar.alimert@gmail.com>
2023-12-28 21:04:04 +0300

    exe.py: German variable names have been changed with English. 	Adapted the code back to using user defined calculation steps.

--- exe.py
+++ exe.py
@@ -12,38 +12,34 @@ from taillib.correction import shift_start_energy
 from taillib.bin import bin_distribution
 from taillib.constants import ehinev
 
-def calc(isotopologue, estart, eend, corr, fname):
+def calc(isotopologue, estart, eend, iStep, corr, fname):
         
-    print("Calculation starts.")
-    print("")
-
     ionization_energies = {"T2":15.486, "DT":15.470, "HT":15.433} # T2: Phs.Rev. A 60(4).3013 (1999)
                                                                   # HT and DT are actually given as D2 and H2 in the paper.
                                                                   # They need re-calculation. (?)
 
-    enanf = estart / ehinev
-    enend = eend / ehinev
+    startingEnergy = estart / ehinev
+    endingEnergy = eend / ehinev
 
     if isotopologue in ionization_energies:
-        ethrsd = ionization_energies[isotopologue] / ehinev
+        energyThreshold = ionization_energies[isotopologue] / ehinev
 
-    enstep = 0.5 / ehinev 
-    enueb = enanf - enstep
-    istep = int((enend-enanf) / enstep) + 1
+    energyStep = (endingEnergy - startingEnergy) / iStep 
+    enueb = startingEnergy - energyStep
 
     result_data = []
 
-    for i in range(1, int(istep)):
-        eprime = enueb - ethrsd
-        if eprime <= 0:
-            eprime = 1.0e-12
-        enueb += enstep 
-        enres = enueb * ehinev
+    for i in range(1, iStep + 1):
+        ePrime = enueb - energyThreshold
+        if ePrime <= 0:
+            ePrime = 1.0e-12
+        enueb += energyStep 
+        energyResult = enueb * ehinev
         # Calculating probabilities with tail function
-        probability = tail(eprime, ethrsd, enueb, enstep)
+        probability = tail(ePrime, energyThreshold, enueb, energyStep)
         # Calculation of fractional recoil momentum with function ktilde
-        fracRecoil = ktilde(enres)
-        result_data.append([enres, probability, fracRecoil])
+        fracRecoil = ktilde(energyResult)
+        result_data.append([energyResult, probability, fracRecoil])
     
     header = ["Energy [eV]", "Prob. Density [eV^-1]", "Frac. Rec. Mom. [a_0^(-1)]"]    
     dfResult = pd.DataFrame(result_data, columns=['e', 'p', 'k'])
@@ -58,18 +54,18 @@ def calc(isotopologue, estart, eend, corr, fname):
         # No correction applied
         fname += "_noCorr"
         print(" ")
-        print("Fractional recoil momentum shift is NOT applied!")
-        print(" ")
+        print("Fractional recoil momentum shift is NOT applied! \n \n")
     
     #binned_final_distribution = bin_distribution(dfResult, bin_size)
     #binned_final_distribution['Frac. Rec. Mom. [a_0^-1]'] = binned_final_distribution['Energy [eV]'].apply(ktilde)
 
     dfResult.to_csv(f"out/{fname}.fsd", sep="\t", index=False, header=header)
-    
-    print("")
+
+    #pd.set_option('display.max_rows', None) # Print the data without truncating the lines
+    # Should we print the full result on the terminal? Or not? I'm note sure. So, for now, it is printed but will be truncated
     print(f"Results have been written into {fname}.fsd file. \n")
-    print(dfResult)
-    print("exe.py and run_tail_ktilde.py executed gracefully.")
-    print("")
+    print(dfResult) #.to_string(index=False)) # If we truncate it, index part is neccessary. If not, removing the index is good idea, I think
+    print("\n \n")
+    print("exe.py and tail_fsd.py executed gracefully. \n")
 
 #   END OF CODE
