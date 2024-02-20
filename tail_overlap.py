49d240dd2c6c1a0987e575d67892ed0c39686285
alimert-t <turaclar.alimert@gmail.com>
2024-02-20 11:46:01 +0100

    tail_overlap.py: moved argument parsing to main() and created new 		 function two handle data saving

--- tail_overlap.py
+++ tail_overlap.py
@@ -72,81 +72,92 @@ def calculate_overlaps(pot_diff_ev):
     overlap_matrix_elements = [overlap.overlap(energy, energy) for energy in pot_diff_ev]
     return overlap_matrix_elements
 
-# Taking and parsing arguments
-parser = argparse.ArgumentParser(
-    description='Calculates the tail overlaps for FSD calculations.',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-p', '--parent-potential-file', type=str, required=True, help='File with parent molecule poential')
-parser.add_argument('-d', '--daughter-potential-file', type=str, required=True, help='File with daughter molecule potential')
-parser.add_argument('-r', '--r-independent', action='store_true', help='Flag to indicate R-independent calculation. If set, a single R value is used for all calculations.')
-parser.add_argument('-v', '--r-value', type=float, help='The R value to use for R-independent calculations. Required if --r-independent is set.')
-parser.add_argument('-n', '--file-name', type=str, help='Name of the calculated overlap file, recomended fashion: <DaughterMolecule>+<ElectronicState><T00X>, i.e., HeTppst001T001. Do NOT include file extension.')
-
-args = parser.parse_args()
-
-file_name = args.file_name + ".snm"
-file_path = 'out/' + file_name
-
-print(' ')
-print(' ')
-print('           *****************************')
-print('           ***                       ***')
-print('           ***                       ***')
-print('           ***      TAIL_OVERLAP     ***')
-print('           ***                       ***')
-print('           ***                       ***')
-print('           *****************************')
-print(' ')
-print(' ')
-
-
-# Check if the file exists, if it does, ask what to do.
-if os.path.exists(file_path):
-    # Prompt the user to confirm overwriting
-    response = input(f"     The file {file_name} already exists. Do you want to overwrite it? (yes/no): ")
-    
-    if response.lower() not in ['yes', 'y']:
-        print("     Calculation cancelled. The existing file will not be overwritten.")
-        print("     quitting...")
-        sys.exit()
-
-num_points = len(overlap_matrix_elements)
-
-# Create the header string, conventinal file type for amo group codes
-header = """
-*
+def save_results(file_path, daughter_data, overlap_matrix_elements):
+    num_points = len(overlap_matrix_elements)
+    header = """
 *
 *      Electronic coupling matrix elements (mixed overlaps)
-*      S_n (R) calculated from atomic tail formula using
-*      subroutines overlap.py and tail.py (atomic tail). 
-*
-*
+*      S_n (R) calculated from atomic tail formula.
 *
        Number of points:     {num_points}
 *
 *      R [a.u.]      Coupling [a.u.]
 *      --------     -----------------
 *
-"""
+""".format(num_points=num_points)
 
-save_data = pd.DataFrame({'R [a.u.]': daughter_data[0], 'Coupling [a.u.]': overlap_matrix_elements})
+    save_data = pd.DataFrame({'R [a.u.]': daughter_data[0], 'Coupling [a.u.]': overlap_matrix_elements})
+    with open(file_path, 'w') as f:
+        f.write(header)
+        save_data.to_csv(f, sep='\t', index=False, header=False, float_format='%f')
+    return save_data, header, num_points
 
-print("\n")
-print(save_data.to_string(index=False)) # Print the result to the console
+def main():
+    print(' ')
+    print(' ')
+    print('           *****************************')
+    print('           ***                       ***')
+    print('           ***                       ***')
+    print('           ***      TAIL_OVERLAP     ***')
+    print('           ***                       ***')
+    print('           ***                       ***')
+    print('           *****************************')
+    print(' ')
+    print(' ')
 
-# Save the overlaps to a file, it is a 3 line code instead of a single line
-# because of the header format. df.to_csv doesn't accept this type of header
-# thus, using with open.
-with open('out/' + file_name, 'w') as f:
-    f.write(header.format(num_points=num_points))
-    save_data.to_csv(f, sep='\t', index=False, header=False, float_format='%f')
+    # Taking and parsing arguments
+    parser = argparse.ArgumentParser(
+        description='Calculates the tail overlaps for FSD calculations.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-p', '--parent-potential-file', type=str, required=True, help='File with parent molecule poential')
+    parser.add_argument('-d', '--daughter-potential-file', type=str, required=True, help='File with daughter molecule potential')
+    parser.add_argument('-r', '--r-independent', action='store_true', help='Flag to indicate R-independent calculation. If set, a single R value is used for all calculations.')
+    parser.add_argument('-v', '--r-value', type=float, help='The R value to use for R-independent calculations. Required if --r-independent is set.')
+    parser.add_argument('-n', '--file-name', type=str, help='Name of the calculated overlap file, recomended fashion: <DaughterMolecule>+<ElectronicState><T00X>, i.e., HeTppst001T001. Do NOT include file extension.')
 
-if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
-    print(" ")
-    print("     Tail overlaps have been calculated successfully! Overlaps have been saved to the file: \n")
-    print(f"                {file_name} \n")
-    print("tail_overlap exiting gracefully. \n")
-else:
-    print(f"    ***Error: Tail overlap {file_name} was not created or is empty. Please check for issues. \n")
+    args = parser.parse_args()
+
+    file_name = args.file_name + ".snm"
+    file_path = 'out/' + file_name
+
+    # Check if the file exists, if it does, ask what to do.
+    if os.path.exists(file_path):
+        # Prompt the user to confirm overwriting
+        response = input(f"     The file {file_name} already exists. Do you want to overwrite it? (yes/no): ")
+    
+        if response.lower() not in ['yes', 'y']:
+            print("     Calculation cancelled. The existing file will not be overwritten.")
+            print("     quitting...")
+            sys.exit()
+
+    daughter_data, parent_data, pot_diff_ev = process_potentials(args.parent_potential_file, args.daughter_potential_file, args.r_independent, args.r_value)
+    overlap_matrix_elements = calculate_overlaps(pot_diff_ev)
+    save_data, header, num_points = save_results(file_path, daughter_data, overlap_matrix_elements)
+
+    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+        print(f"     Tail overlaps have been calculated successfully and saved to {file_name}")
+    else:
+        print(f"    *** Error: Tail overlap file {file_name} was not created or is empty. Please check for issues.")
+
+    print("\n")
+    print(save_data.to_string(index=False)) # Print the result to the console
+
+    # Save the overlaps to a file, this part is a 3 line of codes instead of a single line
+    # because of the header format. df.to_csv doesn't accept this type of header
+    # thus, doing it using `with open`.
+    with open('out/' + file_name, 'w') as f:
+        f.write(header.format(num_points=num_points))
+        save_data.to_csv(f, sep='\t', index=False, header=False, float_format='%f')
+
+    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+        print(" ")
+        print("     Tail overlaps have been calculated successfully! Overlaps have been saved to the file: \n")
+        print(f"                {file_name} \n")
+        print("tail_overlap exiting gracefully. \n")
+    else:
+        print(f"    ***Error: Tail overlap {file_name} was not created or is empty. Please check for issues. \n")
+
+if __name__ == '__main__':
+    main()
 
 #   END OF CODE
